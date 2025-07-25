1c4e46c1ca2b93e822c2a38a83258c9a885a3127
Hrishi <rathodhrishi12345@gmail.com>
2025-07-25 20:57:17 +0530

    Cart add button interactive

--- javascript-amazon-project-main/Scripts/amazon.js
+++ javascript-amazon-project-main/Scripts/amazon.js
@@ -707,7 +707,7 @@ products.forEach((product)=>{
             Added
           </div>
 
-          <button class="add-to-cart-button button-primary">
+          <button class="add-to-cart-button button-primary js-add-to-cart" data-product-id="${product.id}">
             Add to Cart
           </button>
         </div>
@@ -715,4 +715,25 @@ products.forEach((product)=>{
     console.log(productsHTML);
 });
 
-document.querySelector('.products-grid').innerHTML=productsHTML;
+document.querySelector('.js-products-grid').innerHTML=productsHTML;
+document.querySelectorAll('.js-add-to-cart').forEach((button)=>{
+    button.addEventListener('click',()=>{
+        const productId=button.dataset.productId;
+
+        let matchingItem;
+
+        cart.forEach((item)=>{
+            if(productId===item.productId){
+                matchingItem=item;
+            }
+        });
+
+        if(matchingItem){
+            matchingItem.quantity+=1;
+        }else{
+            cart.push({productId: productId,
+            quantity:1
+            });
+        }
+    });
+});
