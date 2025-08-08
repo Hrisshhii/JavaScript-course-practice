fa9506ee383a494d76a85b75e7a97c47a3de5953
Hrishi <rathodhrishi12345@gmail.com>
2025-08-08 22:28:04 +0530

    Save data and generate HTML for delivery options

--- javascript-amazon-project-main/data/cart.js
+++ javascript-amazon-project-main/data/cart.js
@@ -2,9 +2,9 @@ export let cart=JSON.parse(localStorage.getItem('cart'));
 
 if (!cart){
     cart=[{
-    productId:`e43638ce-6aa0-4b85-b27f-e1d07eb678c6`,quantity:2
+    productId:`e43638ce-6aa0-4b85-b27f-e1d07eb678c6`,quantity:2,deliveryOptionID:'1'
 },{
-   productId:`15b6fc6f-327a-4ec4-896f-486349e85a3d`,quantity:2 
+   productId:`15b6fc6f-327a-4ec4-896f-486349e85a3d`,quantity:2,deliveryOptionID:'2'
 }];
 }
 
@@ -25,7 +25,8 @@ export function addToCart(productId){
             matchingItem.quantity+=1;
         }else{
             cart.push({productId: productId,
-            quantity:1
+            quantity:1,
+            deliveryOptionID:'1'
             });
         }
         saveToStorage();
