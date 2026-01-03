02e068f360c75e2c28d1fb1da97ef869ca7c270d
Hrishi <rathodhrishi12345@gmail.com>
2026-01-03 22:48:11 +0530

    create automated test for AddToCart

--- javascript-amazon-project-main/tests-jasmine/utils/moneyTest.js
+++ javascript-amazon-project-main/tests-jasmine/utils/moneyTest.js
@@ -1,4 +1,4 @@
-import {formatCurrency} from '../Scripts/utils/money.js';
+import {formatCurrency} from '../../Scripts/utils/money.js';
 
 describe('test suite for formatCurrency function', () => {
     it('converts cents to dollars', () => {
