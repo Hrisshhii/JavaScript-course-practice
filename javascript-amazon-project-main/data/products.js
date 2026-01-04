d115de29c6311a8d002cda3d2f28eb6fc0d0c92d
Hrishi <rathodhrishi12345@gmail.com>
2026-01-05 00:01:23 +0530

    this keyword properties

--- javascript-amazon-project-main/data/products.js
+++ javascript-amazon-project-main/data/products.js
@@ -58,6 +58,13 @@ class Clothing extends Product{
 // console.log(date);
 // console.log(date.toLocaleTimeString());
 
+// function logThis(){
+//   console.log(this);
+// }
+// logThis();
+// logThis.call('hello');
+
+
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
