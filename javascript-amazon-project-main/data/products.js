05667afd5e9e8560423df877cd0207dce6e378b1
Hrishi <rathodhrishi12345@gmail.com>
2026-01-05 20:40:56 +0530

    Load products from the backend

--- javascript-amazon-project-main/data/products.js
+++ javascript-amazon-project-main/data/products.js
@@ -70,6 +70,25 @@ class Clothing extends Product{
 3. Arrow functions do not have their own "this", they use the "this" from the surrounding context
 */
 
+
+export let products=[];
+export function loadProducts(fun){
+  const xhr=new XMLHttpRequest();
+  xhr.addEventListener('load',()=>{
+    products=JSON.parse(xhr.response).map((productDetails)=>{
+      if(productDetails.type==="clothing"){
+        return new Clothing(productDetails);
+      }
+      return new Product(productDetails);
+    });
+    console.log('Load Products');
+
+    fun();
+  });
+  xhr.open('GET','https://supersimplebackend.dev/products');
+  xhr.send();
+}
+/*
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
@@ -735,4 +754,4 @@ export const products = [
   }
   return new Product(productDetails);
 });
-
+*/
